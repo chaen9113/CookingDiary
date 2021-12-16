0a998b97e7bc5643482d59bca419921d47a2ec88
Jisu Seo <vjsdy5@gmail.com>
2021-12-16 22:11:10 +0900

    SignIn update

--- app/src/main/java/com/termproject/db/myskl/SignInActivity.java
+++ app/src/main/java/com/termproject/db/myskl/SignInActivity.java
@@ -11,6 +11,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -22,6 +23,7 @@ public class SignInActivity extends AppCompatActivity {
     String dbEm = "";
     String dbPw = "";
     DBHelper dbHelper;
+    SQLiteDatabase db;
     boolean pass = false;
 
     @Override
@@ -29,11 +31,15 @@ public class SignInActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
 
-        // DBHelper 생성
-        dbHelper = new DBHelper(this);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
 
-        // 데이터베이스를 읽을 수 있도록 설정
-        SQLiteDatabase db = dbHelper.getReadableDatabase();
+        dbHelper = new DBHelper(this);
+        db = dbHelper.getWritableDatabase();
+
+        if (db == null) {
+            db.execSQL("INSERT INTO INFO VALUES ('" + " " + "', '" + " " + "', '" + " " + "');");
+        }
 
         // Text 입력창 가리키는 변수
         layoutEmail = findViewById(R.id.layout_emailA);
@@ -47,7 +53,7 @@ public class SignInActivity extends AppCompatActivity {
 
         // 로그인 버튼
         Button login = findViewById(R.id.login_button);
-        // 입력하지 않은 초기 상태에 로그인 비활성화
+        // 로그인 정보 입력 전 - 버튼 비활성화
         if (email.isEmpty() || pw.isEmpty()) {
             login.setClickable(false);
             login.setEnabled(false);
@@ -132,12 +138,15 @@ public class SignInActivity extends AppCompatActivity {
                 /* 로그인 동작 */
                 if (pass) {
                     // 데이터 일치하면, id 값을 전달하며 로그인
-                    Intent intent = new Intent(getApplicationContext(), TestActivity.class);
+                    Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
                     intent.putExtra("id", dbEm);
+                    //로그인 한 회원 개인의 테이블로 설정하는 과정. PERSONAL 테이블에 사용자 아이디 값을 넣어준다.
+                    db.execSQL("DELETE FROM PERSONAL");
+                    db.execSQL("INSERT INTO PERSONAL(id) VALUES ('" + dbEm + "');");
                     startActivity(intent);
 
                 } else {
-                    // 일치하지 않으면, 메세지 팝업
+                    // 데이터 일치하지 않으면, 메세지 팝업
                     new AlertDialog.Builder(SignInActivity.this)
                             .setTitle(null)
                             .setMessage("이메일 또는 비밀번호가 일치하지 않습니다.\n입력한 내용을 다시 확인해 주세요.")
