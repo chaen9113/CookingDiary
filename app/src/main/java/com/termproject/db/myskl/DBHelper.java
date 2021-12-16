890481b32f4993376efddde380d1e8de275936dd
Chaehyun Lee <cogusdl1113@gmail.com>
2021-12-16 22:31:47 +0900

    DBHelper 수정

--- app/src/main/java/com/termproject/db/myskl/DBHelper.java
+++ app/src/main/java/com/termproject/db/myskl/DBHelper.java
@@ -1,9 +1,10 @@
-package com.termproject.db.myskl;
+package com.termproject.cookingdiary;
 
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+//데이터베이스 스키마 생성
 public class DBHelper extends SQLiteOpenHelper {
     public static final String DATABASE_NAME = "database";
     public static final int DATABASE_VERSION = 1;
@@ -28,18 +29,17 @@ public class DBHelper extends SQLiteOpenHelper {
 
         String sqlPersonal = "CREATE TABLE IF NOT EXISTS PERSONAL ("
                 + "id text primary key references INFO(id), "
-                + "num_recipe integer primary key references RECIPE(num_recipe));";
+                + "count integer);";
         db.execSQL(sqlPersonal);
 
         String sqlIngredient = "CREATE TABLE IF NOT EXISTS INGREDIENT ("
-                + "num_recipe integer primary key references RECIPE(num_recipe), "
-                + "ingredient text primary key, "
-                + "icategory text);";
+                + "num_recipe integer primary key autoincrement references  RECIPE(num_recipe), "
+                + "ingredient text);";
         db.execSQL(sqlIngredient);
 
         String sqlFood = "CREATE TABLE IF NOT EXISTS FOOD ("
-                + "num_recipe integer primary key references RECIPE(num_recipe), "
-                + "food text primary key references RECIPE(food), "
+                + "num_recipe integer primary key autoincrement references RECIPE(num_recipe), "
+                + "food text references RECIPE(food), "
                 + "fcategory text);";
         db.execSQL(sqlFood);
     }
